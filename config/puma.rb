4bc48aff024adb0196b75ca5856b88fde79c915a
Petrus Repo <petrus.repo@iki.fi>
2018-10-31 11:34:40 +0200

    Set default PORT to be different from voting-api

--- config/puma.rb
+++ config/puma.rb
@@ -14,9 +14,7 @@ end
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests, default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
+port        ENV.fetch("PORT")
 
 # Specifies the `environment` that Puma will run in.
 #
