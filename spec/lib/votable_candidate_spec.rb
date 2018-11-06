f4753499aac5ef269ce3488db5dfc46a378c8e19
Petrus Repo <petrus.repo@iki.fi>
2018-11-06 13:27:34 +0200

    Migrate votable_candidate_spec to current rspec expectation syntax

--- spec/lib/votable_candidate_spec.rb
+++ spec/lib/votable_candidate_spec.rb
@@ -1,8 +1,5 @@
-require 'spec_helper'
-
 describe 'votable behaviour' do
-
-  before(:each) do
+  before do
     stub_result_class!
     @ready_voting_areas = []
     @unready_voting_areas = []
@@ -17,7 +14,6 @@ describe 'votable behaviour' do
   end
 
   describe 'votable candidates' do
-
     it 'gives a list of all candidates ordered by their alliance proportional' do
       allow(AllianceProportional).to receive(:calculate!)
       allow(CoalitionProportional).to receive(:calculate!)
@@ -28,7 +24,10 @@ describe 'votable behaviour' do
       ordered_candidates.should_not be_empty
       ordered_candidates.each_with_index do |candidate, index|
         next_candidate = ordered_candidates[index+1]
-        candidate.alliance_proportionals.last.number.should > next_candidate.alliance_proportionals.last.number if next_candidate
+        next if next_candidate.nil?
+
+        expect(candidate.alliance_proportionals.last.number)
+          .to be > next_candidate.alliance_proportionals.last.number
       end
     end
 
@@ -57,16 +56,20 @@ describe 'votable behaviour' do
       VotableSupport::create_votes_for(alliance.candidates, @ready_voting_areas, :ascending => true)
       VotableSupport::create_votes_for(other_alliance.candidates, @ready_voting_areas, :ascending => true)
 
-      Candidate.count.should > alliance.candidates.count
-      alliance.candidates.with_vote_sums.map(&:id).should == alliance.candidates.reverse.map(&:id)
+      expect(Candidate.count).to be > alliance.candidates.count
+
+      expect(alliance.candidates.with_vote_sums.map(&:id))
+        .to eq alliance.candidates.reverse.map(&:id)
     end
 
     it 'allows chaining with_alliance_proportionals_for with other scopes' do
       result = FactoryGirl.create(:result_with_alliance_proportionals_and_candidates)
       alliance = result.alliance_proportionals.first.candidate.electoral_alliance
-      Candidate.count.should > alliance.candidates.count
 
-      alliance.candidates.with_alliance_proportionals_for(result).map(&:id).should == alliance.candidates.map(&:id)
+      expect(Candidate.count).to be > alliance.candidates.count
+
+      expect(alliance.candidates.with_alliance_proportionals_for(result).map(&:id))
+        .to eq alliance.candidates.map(&:id)
     end
 
     it 'gives a list of all candidates ordered by their vote sum' do
@@ -74,32 +77,37 @@ describe 'votable behaviour' do
       10.times { candidates << FactoryGirl.create(:candidate) }
       VotableSupport::create_votes_for(candidates, @ready_voting_areas, :ascending => true)
 
-      Candidate.with_vote_sums.map(&:id).should == candidates.reverse.map(&:id)
+      expect(Candidate.with_vote_sums.map(&:id))
+        .to eq candidates.reverse.map(&:id)
     end
 
     it 'gives a list of all candidates ordered by their vote sum and excludes unready voting areas' do
       candidates = []
       10.times { candidates << FactoryGirl.create(:candidate) }
       VotableSupport::create_votes_for(candidates, @ready_voting_areas, :ascending => true)
-      VotableSupport::create_votes_for(candidates, @unready_voting_areas, :ascending => false, :base_vote_count => 10000)
+      VotableSupport::create_votes_for(
+        candidates, @unready_voting_areas, :ascending => false, :base_vote_count => 10000
+      )
 
-      Candidate.with_vote_sums.map(&:id).should == candidates.reverse.map(&:id)
+      expect(Candidate.with_vote_sums.map(&:id)).to eq candidates.reverse.map(&:id)
     end
 
-
     describe 'preliminary votes' do
-
-      before(:each) do
+      before do
         @candidate = FactoryGirl.create(:candidate)
       end
 
       it 'has preliminary votes from voting areas which have been fully counted' do
         amount = 10
-        @ready_voting_areas.each { |area| FactoryGirl.create(:vote, :candidate => @candidate,
-                                                                    :voting_area => area,
-                                                                    :amount => amount) }
+        @ready_voting_areas.each do |area|
+          FactoryGirl.create(
+            :vote, :candidate => @candidate,
+            :voting_area => area,
+            :amount => amount
+          )
+        end
 
-        @candidate.votes.preliminary_sum.should == amount * @ready_voting_areas.count
+        expect(@candidate.votes.preliminary_sum).to eq amount * @ready_voting_areas.count
       end
 
       it 'does not add votes from unfinished voting areas to preliminary votes' do
@@ -110,9 +118,8 @@ describe 'votable behaviour' do
                                                              :amount => amount) }
         end
 
-        @candidate.votes.preliminary_sum.should == amount * @ready_voting_areas.count
+        expect(@candidate.votes.preliminary_sum).to eq amount * @ready_voting_areas.count
       end
-
     end
   end
 
@@ -124,9 +131,12 @@ describe 'votable behaviour' do
 
       it 'has preliminary votes from voting areas which have been fully counted' do
         amount = 10
-        @alliance.candidates.each { |c| VotableSupport::create_votes_for_candidate(c, amount, @ready_voting_areas) }
+        @alliance.candidates.each do |c|
+          VotableSupport::create_votes_for_candidate(c, amount, @ready_voting_areas)
+        end
 
-        @alliance.votes.preliminary_sum.should == amount * @ready_voting_areas.count * @alliance.candidates.count
+        expect(@alliance.votes.preliminary_sum)
+          .to eq amount * @ready_voting_areas.count * @alliance.candidates.count
       end
 
       it 'does not count votes from unfinished voting areas to preliminary votes' do
@@ -136,23 +146,26 @@ describe 'votable behaviour' do
           VotableSupport::create_votes_for_candidate(candidate, amount, @unready_voting_areas)
         end
 
-        @alliance.votes.preliminary_sum.should == amount * @ready_voting_areas.count * @alliance.candidates.count
+        expect(@alliance.votes.preliminary_sum)
+          .to eq amount * @ready_voting_areas.count * @alliance.candidates.count
       end
     end
-
   end
 
   describe 'votable coalition behaviour' do
     describe 'preliminary votes' do
-      before(:each) do
+      before do
         @coalition = FactoryGirl.create(:electoral_coalition_with_alliances_and_candidates)
       end
 
       it 'has preliminary votes as a sum of alliance votes' do
         amount = 10
-        @coalition.electoral_alliances.each { |alliance| VotableSupport::create_votes_for_alliance(alliance, amount, @ready_voting_areas) }
+        @coalition.electoral_alliances.each do |alliance|
+          VotableSupport::create_votes_for_alliance(alliance, amount, @ready_voting_areas)
+        end
 
-        @coalition.preliminary_vote_sum.should == amount * @ready_voting_areas.count * @coalition.candidates.count
+        expect(@coalition.preliminary_vote_sum)
+          .to eq amount * @ready_voting_areas.count * @coalition.candidates.count
       end
 
       it 'does not count votes from unfinished voting areas to preliminary votes' do
@@ -162,10 +175,9 @@ describe 'votable behaviour' do
           VotableSupport::create_votes_for_alliance(alliance, amount, @unready_voting_areas)
         end
 
-        @coalition.preliminary_vote_sum.should == amount * @ready_voting_areas.count * @coalition.candidates.count
+        expect(@coalition.preliminary_vote_sum)
+          .to eq amount * @ready_voting_areas.count * @coalition.candidates.count
       end
     end
-
   end
-
 end
