8a5f616ac993207f340b6223bbd94f4426219163
Petrus Repo <petrus.repo@iki.fi>
2025-10-05 17:34:18 +0300

    Update to Rails 7.2.2.2

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Vaalitulostin
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -30,5 +30,8 @@ module Vaalitulostin
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
   end
 end
