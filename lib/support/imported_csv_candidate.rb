02c3243b1e96bde2b20793e98e691d2b574daaf7
Petrus Repo <petrus.repo@iki.fi>
2025-10-05 16:59:55 +0300

    Fix new Zeitwerk load paths

--- lib/support/imported_csv_candidate.rb
+++ lib/support/imported_csv_candidate.rb
@@ -1,37 +1,39 @@
-class ImportedCsvCandidate
+module Support
+  class ImportedCsvCandidate
 
-  attr_accessor :candidate_number,
-                :candidate_name,
-                :lastname,
-                :firstname,
-                :alliance_name
+    attr_accessor :candidate_number,
+                  :candidate_name,
+                  :lastname,
+                  :firstname,
+                  :alliance_name
 
 
-  def self.create_from!(source)
-    imported = build_from source
+    def self.create_from!(source)
+      imported = build_from source
 
-    Candidate.create!(
-      candidate_number: imported.candidate_number,
-      firstname:        imported.firstname,
-      lastname:         imported.lastname,
-      candidate_name:   imported.candidate_name,
-      electoral_alliance: ElectoralAlliance.find_by_name!(imported.alliance_name)
-    )
+      Candidate.create!(
+        candidate_number: imported.candidate_number,
+        firstname:        imported.firstname,
+        lastname:         imported.lastname,
+        candidate_name:   imported.candidate_name,
+        electoral_alliance: ElectoralAlliance.find_by_name!(imported.alliance_name)
+      )
+    end
+
+    def self.build_from(source)
+      new.tap { |imported| imported.convert(source) }
+    end
+
+    # Data is:
+    # Ehdokasnumero,Sukunimi,Etunimi,Ehdokasnimi,Hetu,Puhelin,Email,Postiosoite,Postitoimipaikka,Kaupunki,Vaaliliiton ID,Vaaliliitto,Tiedekuntakoodi,Huomioita
+    #         0       1        2        3         4     5      6     7           8                   9        10          11                12          13
+    def convert(data)
+      @candidate_number = data[0]
+      @lastname         = data[1]
+      @firstname        = data[2]
+      @candidate_name   = data[3]
+      @alliance_name    = data[11]
+    end
+
   end
-
-  def self.build_from(source)
-    new.tap { |imported| imported.convert(source) }
-  end
-
-  # Data is:
-  # Ehdokasnumero,Sukunimi,Etunimi,Ehdokasnimi,Hetu,Puhelin,Email,Postiosoite,Postitoimipaikka,Kaupunki,Vaaliliiton ID,Vaaliliitto,Tiedekuntakoodi,Huomioita
-  #         0       1        2        3         4     5      6     7           8                   9        10          11                12          13
-  def convert(data)
-    @candidate_number = data[0]
-    @lastname         = data[1]
-    @firstname        = data[2]
-    @candidate_name   = data[3]
-    @alliance_name    = data[11]
-  end
-
 end
---
+++ spec/lib/support/imported_csv_alliance_spec.rb
@@ -0,0 +1,41 @@
+require './lib/support/imported_csv_alliance'
+
+module Support
+  RSpec.describe ImportedCsvAlliance, type: :model do
+    describe "Creation" do
+      before(:all) do
+        sep = ","
+        data  = 'Iso Vaaliliitto,1,isohko,50,Akateemiset nallekarhut'
+        @rows = []
+
+        CSV.parse(data, col_sep: sep) do |row|
+          @rows << row
+        end
+      end
+
+      it "builds from csv" do
+        imported_alliance = ImportedCsvAlliance.build_from(@rows.first)
+
+        expect(imported_alliance.name).to eq("Iso Vaaliliitto")
+        expect(imported_alliance.numbering_order).to eq("1")
+        expect(imported_alliance.short_name).to eq("isohko")
+        expect(imported_alliance.candidate_count).to eq("50")
+        expect(imported_alliance.coalition_name).to eq("Akateemiset nallekarhut")
+      end
+
+      it "creates from csv" do
+        FactoryBot.create :electoral_coalition,
+                          name: "Akateemiset nallekarhut"
+
+        alliance = ImportedCsvAlliance.create_from!(@rows.first)
+
+        expect(alliance.class).to eq ElectoralAlliance
+        expect(alliance.name).to eq("Iso Vaaliliitto")
+        expect(alliance.numbering_order).to eq(1)
+        expect(alliance.shorten).to eq("isohko")
+        expect(alliance.electoral_coalition_id)
+          .to eq ElectoralCoalition.find_by(name: "Akateemiset nallekarhut").id
+      end
+    end
+  end
+end
